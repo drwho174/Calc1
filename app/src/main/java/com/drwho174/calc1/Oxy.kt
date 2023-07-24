95e44e31df084daede5844497243a98dc9035834
drwho174 <drwho174@gmail.com>
2023-07-24 09:51:54 +0500

    Oxygen delivery calculator added logic

--- app/src/main/java/com/drwho174/calc1/Oxy.kt
+++ app/src/main/java/com/drwho174/calc1/Oxy.kt
@@ -1,18 +1,112 @@
 package com.drwho174.calc1
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.drwho174.calc1.databinding.FragmentOxyBinding
 
 class Oxy : Fragment() {
+
+    private lateinit var binding: FragmentOxyBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_oxy, container, false)
+    ): View {
+        binding = FragmentOxyBinding.inflate(inflater , container ,false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bsa : EditText = binding.bsa
+        val cardiacoutput: EditText = binding.cardiacoutput
+        val hb: EditText = binding.hb
+        val spo2: EditText = binding.spo2
+        val po2: EditText = binding.po2
+        val etco2: EditText = binding.etco2
+        val freshgasflow: EditText = binding.freshgasflow
+        val arterialcontentO2 : TextView = binding.arterialcontentO2
+        val do2 : TextView = binding.do2
+        val do2i : TextView = binding.do2i
+        val vco2i : TextView = binding.vco2i
+        val do2vco2i : TextView = binding.do2ivco2i
+
+
+
+        //arterial oxygen content calc
+        fun oxygenarterialcontent (): Double {
+            val hbtext = hb.text.toString()
+            val spo2text = spo2.text.toString()
+            val po2text = po2.text.toString()
+            val hbmgdl = hbtext.toDouble()/10
+            return (hbmgdl * 1.34 * spo2text.toDouble()/100) + (po2text.toDouble() * 0.003)
+        }
+
+        //oxygen delivery calc
+
+        fun oxygendelivery(): Double {
+            val cardiacoutputtext = cardiacoutput.text.toString()
+            return cardiacoutputtext.toDouble() * oxygenarterialcontent() * 10
+        }
+
+        fun oxygendelyveryindex(): Double{
+            val bsatext = bsa.text.toString()
+            return oxygendelivery()/ bsatext.toDouble()
+        }
+
+        fun co2productionindex(): Double{
+            val etco2text = etco2.text.toString()
+            val freshgasflowtext = freshgasflow.text.toString()
+            val bsatext = bsa.text.toString()
+            return (etco2text.toDouble() * freshgasflowtext.toDouble() * 1000) /
+                    (bsatext.toDouble() * 760)
+        }
+
+        fun do2indexco2index(): Double{
+            return oxygendelyveryindex() / co2productionindex()
+        }
+
+        val TextWatcher = object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (bsa.text.isNotEmpty() &&
+                    cardiacoutput.text.isNotEmpty() &&
+                    hb.text.isNotEmpty()&&
+                    spo2.text.isNotEmpty()&&
+                    etco2.text.isNotEmpty()&&
+                    po2.text.isNotEmpty()&&
+                    freshgasflow.text.isNotEmpty()){
+
+                    arterialcontentO2.text = oxygenarterialcontent().toString()
+                    do2.text = oxygendelivery().toString()
+                    do2i.text = oxygendelyveryindex().toString()
+                    vco2i.text = co2productionindex().toString()
+                    do2vco2i.text = do2indexco2index().toString()
+
+                }
+            }
+        }
+
+        bsa.addTextChangedListener(TextWatcher)
+        cardiacoutput.addTextChangedListener(TextWatcher)
+        hb.addTextChangedListener(TextWatcher)
+        spo2.addTextChangedListener(TextWatcher)
+        etco2.addTextChangedListener(TextWatcher)
+        po2.addTextChangedListener(TextWatcher)
+        freshgasflow.addTextChangedListener(TextWatcher)
 
     }
 }
